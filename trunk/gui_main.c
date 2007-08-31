8dc2f114b376dfbc72ec94e7df52f13362610c33
ivop <ivop@2f9c504b-9d38-0410-8e56-8c9d6a488007>
2007-08-31 11:08:36 +0000

    suppress stupid/buggy libgtk warning messages to stderr
    it looks like libgtk has no sane mechanism for redirecting stdout/stderr, so
    we just close fd 2 and reopen it to /dev/null
    
    to disable this functionality, export HMGENGUI_SHOW_STDERR= before starting
    the gui

--- trunk/gui_main.c
+++ trunk/gui_main.c
@@ -1,12 +1,23 @@
 #include <gtk/gtk.h>
 #include <locale.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include "gui_interface.h"
 #include "gui_support.h"
 #include "lib_hmgen.c"
 
 int main(int argc, char *argv[]) {
     GtkWidget *hmgengui;
+    char *show_stderr;
+
+    /* stop gtk spamming stderr with warnings about gnome icons
+     * this is a bug in gtk */
+    show_stderr = getenv("HMGENGUI_SHOW_STDERR");
+    if (!show_stderr) {
+        close(2);
+        open("/dev/null", O_WRONLY);
+    }
 
     gtk_disable_setlocale();
     setlocale(LC_ALL, "C");
